dc8c3c6f2fbcb41f709b3d99b5dd65178ab45dad
progre <djyayutto@gmail.com>
2020-11-05 00:16:13 +0900

    fix: warn on release

--- src/debug.rs
+++ src/debug.rs
@@ -1,16 +1,16 @@
-use winapi::um::debugapi::OutputDebugStringW;
-
 #[cfg(not(debug_assertions))]
 pub fn d(_: &str) {}
 
 #[cfg(debug_assertions)]
 pub fn d(msg: &str) {
+    use winapi::um::debugapi::OutputDebugStringW;
+
     let vec = encode(msg);
     unsafe {
         OutputDebugStringW(vec.as_ptr());
     }
-}
 
-fn encode(source: &str) -> Vec<u16> {
-    source.encode_utf16().chain(Some(0)).collect()
+    fn encode(source: &str) -> Vec<u16> {
+        source.encode_utf16().chain(Some(0)).collect()
+    }
 }
